5b9992e60b5d311cccddea32a746cb29ec87637a
Mohammed Header <mohheader@hotmail.com>
2014-07-17 00:58:33 +0200

    Working Collision

--- core/src/com/me/zbirds/GameObjects/Bird.java
+++ core/src/com/me/zbirds/GameObjects/Bird.java
@@ -1,5 +1,6 @@
 package com.me.zbirds.GameObjects;
 
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 
 /**
@@ -14,12 +15,16 @@ public class Bird {
     private int width;
     private int height;
 
+    private Circle boundingCircle;
+
     public Bird(float x, float y, int width, int height) {
         this.width = width;
         this.height = height;
         position = new Vector2(x, y);
         velocity = new Vector2(0, 0);
         acceleration = new Vector2(0, 460);
+
+        boundingCircle = new Circle();
     }
 
     public void update(float delta) {
@@ -31,6 +36,8 @@ public class Bird {
         }
 
         position.add(velocity.cpy().scl(delta));
+
+        boundingCircle.set(position.x + 9, position.y + 6, 6.5f);
 
         // Rotate counterclockwise
         if (velocity.y < 0) {
@@ -79,5 +86,9 @@ public class Bird {
 
     public boolean shouldntFlap() {
         return velocity.y > 70;
+    }
+
+    public Circle getBoundingCircle() {
+        return boundingCircle;
     }
 }
